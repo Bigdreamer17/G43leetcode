3c80abb48450f23acc722d3f88e470b42203cfa9
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2022-12-25 18:16:33 +0300

    Add files via upload

---
+++ set_union.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+english_number = int(input()) 
+english_paper = set(map(int , input().split()))
+
+french_number = int(input()) 
+french_paper = set(map(int , input().split()))
+
+print(len(english_paper.union(french_paper)))
