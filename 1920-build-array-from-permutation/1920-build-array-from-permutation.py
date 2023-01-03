2a36259c2aeae76c880764016329020172175f8e
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2023-01-03 16:12:11 +0300

    Time: 247 ms (47.41%), Space: 14.3 MB (8.30%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -0,0 +1,6 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans = [nums[i] for i in nums]
+        # for i in nums:
+        #     ans.append(nums[i])
+        return ans
