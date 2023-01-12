e5e7c8a305423fb680ccc1d5a8c19c92b1a23b32
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2023-01-12 16:10:07 +0300

    Time: 46 ms (81.79%), Space: 14.4 MB (87.56%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,7 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        tar = []
+        for i in matrix:
+            if i[0] <= target:
+                tar = i
+        return target in tar
