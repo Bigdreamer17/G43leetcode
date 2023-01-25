c1c0df3ad74d1ffac6d4ee91a52e65cd664def08
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2023-01-25 18:18:41 +0300

    Time: 396 ms (43.33%), Space: 13.9 MB (57.13%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -10,22 +10,6 @@ class Solution:
             else:
                 return True
         return False
-
-#         for i in range(c):
-#             if i ** 2 <= c:
-#                 possibles.append(i)
         
-#         # if len(possibles) < 2:
-#         #     return False
-#         ptr1 = 0
-#         ptr2 = 0
         
-#         while ptr1 < len(possibles) or ptr2 < len(possibles) :
-#             if (possibles[ptr1] ** 2) + (possibles[ptr2] ** 2) > c:
-#                 ptr2 += 1
-#             elif (possibles[ptr1] ** 2) + (possibles[ptr2] ** 2) < c:
-#                 ptr1 += 1
-#             else:
-#                 return True
-#         return False
             
