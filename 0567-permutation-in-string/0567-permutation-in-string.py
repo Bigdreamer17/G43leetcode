01992af5d318c24fc8b62e643df359047a25bc2b
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2023-02-23 08:08:47 -0500

    Time: 79 ms (65.25%), Space: 14 MB (23.75%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,29 +1,30 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        # Create a dictionary
         Count1 = {}
         
         for i in s1:
             Count1[i] = 1 + Count1.get(i, 0)
-            
+        # Initialize our pointers, window and dict_count   
         l = r = 0
         window = len(s1)
-        dis = len(Count1)
+        dict_count = len(Count1)
         
         while r < len(s2):
             if s2[r] in Count1:
                 Count1[s2[r]] -= 1
                 if Count1[s2[r]] == 0:
-                    dis -= 1
+                    dict_count -= 1
             if r - l + 1 < window:
                 r += 1
             else:
-                if dis == 0:
+                if dict_count == 0:
                     return True 
                 if s2[l] in Count1:
                     Count1[s2[l]] += 1
                     
                     if Count1[s2[l]] == 1:
-                        dis += 1
+                        dict_count += 1
                 l += 1
                 r += 1
         return False
