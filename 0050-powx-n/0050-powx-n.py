16dce3fba57be1398cd28a9d8b93b49e6e7ab894
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2023-03-15 08:04:42 -0400

    Time: 27 ms (89.84%), Space: 13.8 MB (54.42%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,5 +1,24 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if x == 1:
+        # Base case :- n == 1 return x
+        # recursive relation = 
+        if n == 1:
             return x
-        return x * pow(x, n - 1)
+        if n == 0:
+            return 1
+        if n == -1:
+            return 1 / x
+        
+        if n < 0 and n % 2 == 0:
+            return self.myPow(x * x, n // 2)
+        elif n < 0 and n % 2 != 0:
+            return x * self.myPow(x * x, n // 2)
+        
+        if n % 2 == 0:
+            return self.myPow(x * x, n // 2)
+        else:
+            return x * self.myPow(x * x, n // 2)
+        
+        
+            
+        
