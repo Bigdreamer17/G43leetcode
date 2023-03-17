950b1c145ab87037d3f353bd350321de3ccc5500
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2023-03-17 16:00:56 -0400

    Time: 516 ms (21.74%), Space: 16.1 MB (15.24%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,24 +1,29 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         ans = []
-        integers = [] 
+        
+        integers = []
+        # building our integer array
         for i in range(1, n + 1):
             integers.append(i)
         
         def backtrack(lis, index):
+            # Goal
             if len(lis) == k:
                 ans.append(lis[:])
                 return
-            
+            # Constraints
             if index >= len(integers):
                 return 
             if len(lis) > k:
                 return
-            
+            # Choices
+                # Pick move
             lis.append(integers[index])
             backtrack(lis, index + 1)
             lis.pop()
             
+            # Don't pick move
             backtrack(lis, index + 1)
         backtrack([], 0)
         return ans
