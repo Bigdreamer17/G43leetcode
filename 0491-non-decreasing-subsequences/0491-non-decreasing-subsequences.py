f2c26b89babbf63b6ad0634fa10d3e21b26eabbf
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2023-03-20 11:49:39 -0400

    Time: 278 ms (24.37%), Space: 22.4 MB (51.54%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -1,24 +1,48 @@
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
-        ans = dict()
+        ans = set()
         
         def backtrack(index, lis):
             if len(lis) >= 2 and lis[-2] <= lis[-1]:
-                ans[tuple(lis[:])] = 1
+                ans.add(tuple(lis[:]))
+            
             if len(lis) >= 2 and lis[-2] > lis[-1]:
                 return 
+            
             if index >= len(nums):
                 return 
             
-            # pick and move
+           # pick and move
             lis.append(nums[index])
             backtrack(index + 1, lis)
             lis.pop()
             
             backtrack(index + 1, lis) # Dont pick move
+                
+                
+        backtrack(0, [])
+        return ans
+
+#         ans = dict()
+        
+#         def backtrack(index, lis):
+#             if len(lis) >= 2 and lis[-2] <= lis[-1]:
+#                 ans[tuple(lis[:])] = 1
+            
+            
+#             if index >= len(nums):
+#                 return 
+            
+#             # pick and move
+#             lis.append(nums[index])
+#             backtrack(index + 1, lis)
+#             lis.pop()
+            
+#             backtrack(index + 1, lis) # Dont pick move
             
             
                 
-        backtrack(0, [])
+#         backtrack(0, [])
         
-        return ans
+#         return ans
+    
