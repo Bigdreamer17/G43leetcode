eb385860661c601ac5ca58f0912421f954c13a95
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2023-03-24 15:46:13 -0400

    Time: 406 ms (20.72%), Space: 21.1 MB (91.76%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -15,13 +15,3 @@ class Solution:
             if nums[i] != i + 1:
                     ans.append(nums[i])
         return ans
-    # i=0
-    # while i<len(nums):
-    #     idx=nums[i]-1
-    #     if nums[i]!=i+1 and nums[idx]!=nums[i]:
-    #         nums[i],nums[idx]=nums[idx],nums[i]
-    #     else:
-    #         i+=1
-    # for i in range(len(nums)):
-    #     if nums[i]!=i+1:
-    #         yield nums[i]
