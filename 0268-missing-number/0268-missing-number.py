042be6beedbdf49f48a2fe138e2755ab158e2cf7
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2023-03-25 15:18:20 -0400

    Delete 0268-missing-number.py

--- 0268-missing-number/0268-missing-number.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        for i in range(len(nums) + 1):
-            if i in nums:
-                continue
-            else:
-                return i
