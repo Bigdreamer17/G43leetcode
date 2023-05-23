9d128416b5ad14df4549071b7f6fdf734f5c4ffb
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2023-05-23 22:19:52 +0300

    Time: 36 ms (74.53%), Space: 16.3 MB (35.52%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        def factors(n):
+            d = 2
+            while d * d <= n:
+                while n % d == 0:
+                    n /= d
+                    yield d
+                d += 1
+            if n > 1:
+                yield n
+
+        return int(sum(factors(n)))
