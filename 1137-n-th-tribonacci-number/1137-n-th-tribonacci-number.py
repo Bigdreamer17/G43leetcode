a2464b6bb65875a87ec045551af91a9a73777b95
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2023-05-31 18:40:18 +0300

    Time: 48 ms (24.75%), Space: 16.3 MB (39.87%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,7 +1,18 @@
 class Solution:
-    def __init__(self):
-        self.memo = {0:0,1 : 1, 2 : 1}
     def tribonacci(self, n: int) -> int:
-        if n not in self.memo.keys():
-            self.memo[n] = self.tribonacci(n - 1) + self.tribonacci(n - 2) + self.tribonacci(n - 3)
-        return self.memo[n]
+        # Bottom-up approach
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        if n == 2:
+            return 1
+        dp = [0 for _ in range(n + 1)]
+        
+        dp[0] = 0
+        dp[1] = dp[2] = 1
+        
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3] 
+            
+        return dp[-1]
