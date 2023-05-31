a6604a3008365275bdc20f4d67459d6956f33947
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2023-05-31 18:45:30 +0300

    Time: 45 ms (54.51%), Space: 16.3 MB (7.08%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,10 +1,13 @@
 class Solution:
-    def __init__(self):
-        self.cache = {}
-        
     def fib(self, n: int) -> int:
-        if n <= 1:
-            return n
-        if n not in self.cache.keys():
-            self.cache[n] = self.fib(n - 1) + self.fib(n - 2)
-        return self.cache[n]
+        # Bottom-up approach
+        if n == 0:
+            return 0
+        dp = [0 for _ in range(n + 1)]
+        dp[0] = 0
+        dp[1] = 1
+        
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+            
+        return dp[-1]
