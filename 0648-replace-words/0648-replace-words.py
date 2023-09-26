ccb54b72e4a92617bcb0a6cb1764e484f8dca995
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2023-09-26 21:25:17 +0300

    Time: 125 ms (73.87%), Space: 38 MB (37.42%) - LeetHub

--- 0648-replace-words/0648-replace-words.py
+++ 0648-replace-words/0648-replace-words.py
@@ -1,90 +1,43 @@
-class TrieNode:
-    def __init__(self):
-        self.child = {}
-        self.end = False
-
 class Trie:
     def __init__(self):
         self.root = TrieNode()
+    
     def insert(self, word):
         parent = self.root
         for c in word:
-            if not c in parent.child:
-                parent.child[c] = TrieNode()
-            parent = parent.child[c]
-        parent.end = True
-
+            if not c in parent.children:
+                parent.children[c] = TrieNode()
+            parent = parent.children[c]
+        parent.is_end = True
+        
     def search(self, word):
         parent = self.root
         ans = ''
         for c in word:
-            if not c in parent.child:
+            if not c in parent.children:
                 return word
             ans += c
-            parent = parent.child[c]
-            if parent.end == True:
+            parent = parent.children[c]
+            if parent.is_end == True:
                 return ans
         return word
-            
+    
+class TrieNode:
+    def __init__(self):
+        self.is_end = False
+        self.children = {}
+
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        graph = Trie()
-        list_word = sentence.split()
-        for dic in dictionary:
-            graph.insert(dic)
+        trie = Trie()
+        
+        for word in dictionary:
+            trie.insert(word)
+        
+        sent = sentence.split()
         ans = []
-        for word in list_word:
-            ans.append(graph.search(word))
         
+        for word in sent:
+            ans.append(trie.search(word))
+            
         return " ".join(ans)
-
-
-
-
-
-
-
-
-
-# class Trie:
-#     def __init__(self):
-#         self.root = TrieNode()
-    
-#     def insert(self, word):
-#         cur = self.root
-        
-#         for wd in word:
-#             index = ord(wd) - ord('a')
-#             if cur.children[index] is None:
-#                 cur.children[index] = TrieNode()
-                
-#             cur = cur.children[index]
-#         cur.is_end = True
-        
-#     def startwith(self, prefix: str) -> bool:
-#         cur = self.root
-#         for ch in word:
-#             indx = ord(ch) - ord('a')
-#             if cur.children[indx] is None:
-#                 return False
-
-#             cur = cur.children[indx]
-#         return True
-    
-# class TrieNode:
-#     def __init__(self):
-#         self.is_end = False
-#         self.children = [ None for _ in range(26)]
-
-# class Solution:
-#     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-#         trie = Trie()
-        
-#         for word in dictionary:
-#             trie.insert(word)
-        
-#         sen = sentence.split()
-    
-#         for indx, sent in enumerate(sen):
-#             if trie.startwith(sent):
-#                 sen[indx] = 
