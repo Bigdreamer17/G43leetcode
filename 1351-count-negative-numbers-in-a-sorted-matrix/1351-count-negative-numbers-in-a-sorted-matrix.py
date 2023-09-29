e5b2e3cbdceda1f2c09b47e3f598e2f6da3d43a6
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2023-09-29 20:42:30 +0300

    Time: 110 ms (80.72%), Space: 17.5 MB (55.81%) - LeetHub

---
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        ans = 0
+        def binary_search(row):
+            start, end = 0, len(row)
+            while start<end:
+                mid = start +(end -start) // 2
+                if row[mid]<0:
+                    end = mid
+                else:
+                    start = mid+1
+            return len(row)- start
+        
+        count = 0
+        for row in grid:
+            count += binary_search(row)
+        return(count)
+        
