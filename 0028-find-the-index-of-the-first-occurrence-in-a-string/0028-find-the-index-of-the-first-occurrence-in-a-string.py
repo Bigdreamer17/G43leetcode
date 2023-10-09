5411029ac8f5d45059ffd376418c8c2de65f1a90
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2023-10-09 16:28:08 +0300

    Update 0028-find-the-index-of-the-first-occurrence-in-a-string.py
    Added the KMP Approach

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -11,3 +11,42 @@ class Solution:
                     return window_start
 
         return -1
+
+# Using KMP
+class Solution:
+    def KMP(self, needle):
+            n = len(needle)
+            l , r = 0 , 1 
+            LPS = [0 for _ in range(n)]
+            
+            while r < n:
+                if needle[l] == needle[r]:
+                    LPS[r] = l + 1
+                    l += 1
+                    r += 1
+                    
+                else:
+                    if l == 0:
+                        r += 1
+                    else:
+                        l = LPS[l - 1]
+            return LPS
+    def strStr(self, haystack: str, needle: str) -> int:
+        
+        lsp = self.KMP(needle)
+        p1 = p2 = 0
+        
+        while p1 < len(haystack):
+            if haystack[p1] == needle[p2]:
+                p1 += 1
+                p2 += 1
+            elif p2 == 0:
+                p1 += 1
+            else:
+                p2 = lsp[p2 - 1]
+                
+            
+            if p2 == len(needle):
+                return p1 - p2
+            
+        return -1
