2f2a5593a4c282c7618f118340c8179dbe6d211b
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2023-10-30 17:31:16 +0300

    Added solution for Design HashMap

---
+++ leetcode-solution/design-hashmap.py
@@ -0,0 +1,19 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.valueList= [-1] * ((10**6) + 1)
+
+    def put(self, key: int, value: int) -> None:
+        self.valueList[key] = value
+
+    def get(self, key: int) -> int:
+        return self.valueList[key]
+    def remove(self, key: int) -> None:
+        self.valueList[key] = -1
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
