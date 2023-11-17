fb44f2f09f9d4020c0ade6da35aa75389f14815c
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2023-11-17 20:37:38 +0300

    Added solution for Unique Number of Occurrences

--- leetcode-solution/unique-number-of-occurrences.py
+++ leetcode-solution/unique-number-of-occurrences.py
@@ -1,14 +1,16 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        freq = {}
-        for a in arr:
-            if a not in freq:
-                freq[a] = 1
-            freq[a] += 1
-        values = []
-        for v in freq.values():
-            values.append(v)
-        if len(set(values)) == len(set(arr)):
-            return True
-        else:
-            return False
+        occ = {}
+        for num in arr:
+            if num not in occ:
+                occ[num] = 1
+            occ[num] += 1
+
+        key_ = []
+        value_ = []
+
+        for key, value in occ.items():
+            key_.append(key)
+            value_.append(value)
+
+        return len(set(value_)) == len(key_)
